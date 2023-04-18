8df7e4784df2d90bb7f121fbdf74db8327cf4b03
Piotr Płaczek <piotr.placzek.91@gmail.com>
2023-04-18 10:07:27 +0200

    add multiproject support

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const TIMEZONE: string = process.env.TIMEZONE || 'Europe/Warsaw';
 export const REPORTS_STORAGE_PATH = process.env.REPORTS_STORAGE_PATH || './reports';
-export const PROJECT_NAME: string = process.env.PROJECT_NAME || 'General';
+export const PROJECTS: string[] = process.env.PROJECTS?.split(',') || ['General'];
 export const WAKATIME_API_KEY: string = process.env.WAKATIME_API_KEY || '';
 export const EMPLOYEE_NAME: string = process.env.EMPLOYEE_NAME || '';
 export const BUSINESS_DAYS: number[] = process.env.BUSINESS_DAYS?.split(',').map((d) => +d);
